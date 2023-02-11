42c09a4d5a35f53d4a87ce27400d8e5adc9bf48f
hex benjamin <64101028+hexbenjamin@users.noreply.github.com>
2023-02-11 05:12:50 +0000

    Initial commit

---
+++ .build/vite.config.js
@@ -0,0 +1,21 @@
+var __defProp = Object.defineProperty;
+var __markAsModule = (target) => __defProp(target, "__esModule", { value: true });
+var __export = (target, all) => {
+  __markAsModule(target);
+  for (var name in all)
+    __defProp(target, name, { get: all[name], enumerable: true });
+};
+__export(exports, {
+  default: () => vite_config_default
+});
+var vite_config_default = {
+  server: {
+    host: "0.0.0.0",
+    hmr: {
+      port: 443
+    }
+  }
+};
+// Annotate the CommonJS export names for ESM import in node:
+0 && (module.exports = {});
+//# sourceMappingURL=vite.config.js.map
